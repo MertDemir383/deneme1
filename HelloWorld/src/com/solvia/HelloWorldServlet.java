04b635dc06f0bdda97fc4e84c5459ebffa9e7b22
Solvia <Solvia@172.16.10.125>
2018-08-08 13:34:56 +0300

    New commit

--- HelloWorld/src/com/solvia/HelloWorldServlet.java
+++ HelloWorld/src/com/solvia/HelloWorldServlet.java
@@ -30,6 +30,7 @@ public class HelloWorldServlet extends HttpServlet {
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		response.getWriter().println("Hello World");
 		response.getWriter().println("Hello Solvia");
+		response.getWriter().println("Hello Mert");
 		
 	}
 
